26b37aad0f54dcc09eb6d6521e5cea4800c8d45e
GetExpSV <savinykhvladislav@gamil.com>
2020-04-28 20:11:52 +0300

    Updates

---
+++ precache-manifest.592e286c762c996aec8dd78d361034f2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9f8a97d19c06201c71fb50b833a699",
+    "url": "/testdeploy/index.html"
+  },
+  {
+    "revision": "fa56a20c74ef804c421a",
+    "url": "/testdeploy/static/css/main.019106ac.chunk.css"
+  },
+  {
+    "revision": "77132edf161b7bc43d24",
+    "url": "/testdeploy/static/js/2.0a548c36.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/testdeploy/static/js/2.0a548c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa56a20c74ef804c421a",
+    "url": "/testdeploy/static/js/main.794d916c.chunk.js"
+  },
+  {
+    "revision": "87c82223f90bb5ae3647",
+    "url": "/testdeploy/static/js/runtime-main.9d0e9528.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/testdeploy/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/testdeploy/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/testdeploy/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/testdeploy/static/media/menu icon.6f6bef28.svg"
+  }
+]);
